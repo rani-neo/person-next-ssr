b355ef2963ce6f763f252bce99d9d3166e860b78
rani-neo <112041804+rani-neo@users.noreply.github.com>
2024-04-20 21:17:39 +1000

    Update withAuth.ts

--- utils/withAuth.ts
+++ utils/withAuth.ts
@@ -1,33 +1,27 @@
-import { useAuthenticator } from '@aws-amplify/ui-react-core';
-import { Auth } from 'aws-amplify';
-import { useNavigation } from 'next/navigation'; // Use next/navigation instead of next/router
 import { useEffect } from 'react';
-import React from 'react';
+import { useRouter } from 'next/router';
+import { Auth } from 'aws-amplify';
 
-const withAuth = (WrappedComponent: any) => {
-    const WrapperComponent = (props: any) => {
-        const navigation = useNavigation(); // Use useNavigation hook
+const withAuth = (WrappedComponent: React.ComponentType) => {
+  const RequireAuth: React.FC<any> = (props) => {
+    const router = useRouter();
 
-        if (typeof window !== 'undefined') {
-            const { user } = useAuthenticator((context: { user: any }) => [context.user]);
-
-            useEffect(() => {
-                const checkAuth = async () => {
-                    try {
-                        await Auth.currentAuthenticatedUser();
-                    } catch (err) {
-                        navigation.navigate('/login'); // Use navigation.navigate instead of router.push
-                    }
-                };
-
-                checkAuth();
-            }, [navigation, user]);
+    useEffect(() => {
+      const checkAuth = async () => {
+        try {
+          await Auth.currentAuthenticatedUser();
+        } catch (error) {
+          router.push('/signin'); // Redirect to the sign-in page if not authenticated
         }
+      };
 
-        return <WrappedComponent {...props} />;
-    };
+      checkAuth();
+    }, []);
 
-    return WrapperComponent;
+    return <WrappedComponent {...props} />;
+  };
+
+  return RequireAuth;
 };
 
 export default withAuth;
